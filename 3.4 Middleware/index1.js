84d9655902eadc5ab1bb3646c7b26d5f02608fde
Eugenia <1936949995@qq.com>
2024-08-28 16:11:39 +0930

    first commit

---
+++ 3.4 Middleware/index1.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+//动态地确定文件路径，不需要输入完整的路径
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+});
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
